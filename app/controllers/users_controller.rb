9454517468a39ea65d7ea7cbac239a424f01e3c3
kmarks2013 <kmarks2013@gmail.com>
2020-04-22 19:37:01 -0400

    adds private user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
         user = User.create()
     end
 
+    private
+    def user_params
+        params.permit(:firstname, :lastname, :username, :password, :address, :email, :phone)
+    end
+
 end
