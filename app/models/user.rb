c8ff95ae7a3e7fd07a0d851c0e6458a091d2d54f
kmarks2013 <kmarks2013@gmail.com>
2020-04-22 19:57:59 -0400

    adds first and last name validations presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     has_secure_password
     valdiates_presence_of :username
     validates_uniqueness_of :username, :case_sensitive => false
+    # validates :firstname, :presence => true
+    # validates :lastname, :presence => true
 
 end
