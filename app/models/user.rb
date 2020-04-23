f79d77333af4bcbc4318848c798c85592d8bd1c6
kmarks2013 <kmarks2013@gmail.com>
2020-04-22 20:42:57 -0400

    adds length validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     has_secure_password
     valdiates_presence_of :username
     validates_uniqueness_of :username, :case_sensitive => false
+    validates :username, :length => {:in => 6..15}
     # validates :firstname, :presence => true
     # validates :lastname, :presence => true
 
