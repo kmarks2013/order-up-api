3d9ed28d753b79f6c7069b7243c95d6592244236
kmarks2013 <kmarks2013@gmail.com>
2020-04-22 20:59:20 -0400

    adds password formrat validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
     /x
     has_secure_password
     validates :password, :length => {:minimum => 6 }
+    validates :password, format: {with: PASSWORD_FORMAT, :message =>'Password must include: 1 uppercase, 1 lowercase, 1 digit, and 1 special character.'}
+
     #username validations
     validates_presence_of :username
     validates_uniqueness_of :username, :case_sensitive => false
