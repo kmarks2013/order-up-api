59d184e5f3421e39f6e1b8e8d17c5381bbd11097
kmarks2013 <kmarks2013@gmail.com>
2020-04-22 20:48:07 -0400

    changes username length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_secure_password
     valdiates_presence_of :username
     validates_uniqueness_of :username, :case_sensitive => false
-    validates :username, :length => {:in => 6..15}
+    validates :username, :length => {:in => 6..25}
     # validates :firstname, :presence => true
     # validates :lastname, :presence => true
 
