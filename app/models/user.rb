82b6dc3e8e4b7eec05e87066eba272c67fa038ad
kmarks2013 <kmarks2013@gmail.com>
2020-04-22 20:48:53 -0400

    adds comments seperating username and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    #validations
-    has_secure_password
-    valdiates_presence_of :username
+    # password validations
+    has_secure_passwor
+    #username validations
+    validates_presence_of :username
     validates_uniqueness_of :username, :case_sensitive => false
     validates :username, :length => {:in => 6..25}
     # validates :firstname, :presence => true
